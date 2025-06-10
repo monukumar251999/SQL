9a958d269fe2b49804eab456abfdbafe19189877
monukumar251999 <59949759+monukumar251999@users.noreply.github.com>
2025-06-10 21:53:45 +0530

    Update Customer_Retention_churn_part1.sql

--- Customer_Retention_churn_part1.sql
+++ Customer_Retention_churn_part1.sql
@@ -7,11 +7,18 @@ Wallet cash back (paytm/gpay)
 Zomato pro swiggy super
 Retention period
 */
-
+-- part 1
 select month(this_month.order_date)  as month_date,count(distinct last_month.cust_id)from transactions this_month
 left join transactions last_month on this_month.order_date = last_month.order_date
 and datediff(month,this_month.order_date,last_month.order_date)=1
 group by month(this_month.order_date)
+
+-- part 2
+select month(last_month.order_date)  as month_date,count(distinct last_month.cust_id)from transactions last_month
+left join transactions this_month on this_month.order_date = last_month.order_date
+and datediff(month,last_month.order_date,this_month.order_date)=1
+where this_month.cust_id is null
+group by month(last_month.order_date)
 
 
 
